3b91df375e9d0a8421baa212643bdcfd5f0793f6
Christian Meredith <ceigey@users.noreply.github.com>
2026-07-19 02:47:55 +1000

    Initial art and targeting concept

--- main.lua
+++ main.lua
@@ -1,21 +1,125 @@
 function _config()
   ---@type Usagi.Config
-  return { name = "Game", game_id = "com.usagiengine.YOURGAMENAME" }
+  return {
+    name = "Game",
+    game_id = "com.usagiengine.YOURGAMENAME",
+    game_height = 180 * 2,
+    game_width = 320 * 2,
+    sprite_size = 32,
+  }
 end
 
 function _init()
   -- Live reload preserves globals across saved edits but resets locals.
   -- Stash mutable game state in a capitalized global like `State` so it
   -- survives reloads; F5 calls _init again to reset.
-  State = {}
+  State = {
+    player = {
+      rotation = 3.14 / 4
+    },
+    mouse = {
+      position = {
+        x = 0,
+        y = 0,
+      }
+    }
+  }
 end
 
 ---@param dt number
 function _update(dt)
+  local mouse_x, mouse_y = input.mouse()
+
+  State.player.position = {
+    x = usagi.GAME_W / 2 - usagi.SPRITE_SIZE / 2,
+    y = usagi.GAME_H / 2 - usagi.SPRITE_SIZE / 2
+  }
+
+  State.mouse = State.mouse or {}
+  State.mouse.position = {
+    x = mouse_x,
+    y = mouse_y
+  }
+
+  -- because I'm forgetful
+  -- https://gamedev.stackexchange.com/questions/14602/what-are-atan-and-atan2-used-for-in-games
+  local rotation = math.atan(
+    State.mouse.position.y - State.player.position.y,
+    State.mouse.position.x - State.player.position.x
+  )
+  State.player.rotation = rotation
+
+  -- How to angle: https://stackoverflow.com/a/839931
+  -- Whatever I have below actually works well for projecting beneath
+  local sightline_start_radius = 32
+  local sightline_end_radius = 320 + 180
+  local sightline_spread = math.pi / 32
+  State.sightlines = State.sightlines or {}
+  State.sightlines.left = {
+    startpoint = {
+      x = State.player.position.x + sightline_start_radius * math.cos(State.player.rotation - sightline_spread * 2),
+      y = State.player.position.y + sightline_start_radius * math.sin(State.player.rotation - sightline_spread * 2),
+    },
+    endpoint = {
+      x = State.player.position.x + sightline_end_radius * math.cos(State.player.rotation - sightline_spread),
+      y = State.player.position.y + sightline_end_radius * math.sin(State.player.rotation - sightline_spread),
+    },
+  }
+  State.sightlines.right = {
+    startpoint = {
+      x = State.player.position.x + sightline_start_radius * math.cos(State.player.rotation + sightline_spread * 2),
+      y = State.player.position.y + sightline_start_radius * math.sin(State.player.rotation + sightline_spread * 2),
+    },
+    endpoint = {
+      x = State.player.position.x + sightline_end_radius * math.cos(State.player.rotation + sightline_spread),
+      y = State.player.position.y + sightline_end_radius * math.sin(State.player.rotation + sightline_spread),
+    },
+  }
 end
 
 ---@param dt number
 function _draw(dt)
-  gfx.clear(gfx.COLOR_BLACK)
-  gfx.text("Hello, Usagi!", 10, 10, gfx.COLOR_WHITE)
+  gfx.clear(gfx.COLOR_WHITE)
+
+  -- gfx.line_ex(
+  --   State.player.position.x,
+  --   State.player.position.y,
+  --   State.mouse.position.x,
+  --   State.mouse.position.y,
+  --   1,
+  --   gfx.COLOR_RED, 0.25
+  -- )
+
+  gfx.line_ex(
+    State.sightlines.left.startpoint.x,
+    State.sightlines.left.startpoint.y,
+    State.sightlines.left.endpoint.x,
+    State.sightlines.left.endpoint.y,
+    1,
+    gfx.COLOR_RED, 0.25
+  )
+  gfx.line_ex(
+    State.sightlines.right.startpoint.x,
+    State.sightlines.right.startpoint.y,
+    State.sightlines.right.endpoint.x,
+    State.sightlines.right.endpoint.y,
+    1,
+    gfx.COLOR_RED, 0.25
+  )
+
+  gfx.spr_ex(
+    1,
+    State.player.position.x - usagi.SPRITE_SIZE / 2,
+    State.player.position.y - usagi.SPRITE_SIZE / 2,
+    false,
+    false,
+    State.player.rotation + math.pi / 2,
+    gfx.COLOR_TRUE_WHITE, 1.0
+  )
+
+
+  gfx.circ_fill(State.mouse.position.x, State.mouse.position.y, 4, gfx.COLOR_RED, 0.5)
+  gfx.circ(State.mouse.position.x, State.mouse.position.y, 6, gfx.COLOR_RED, 0.5)
+
+  gfx.text("Hello, Usagi!", 10, 10, gfx.COLOR_BLACK)
 end
